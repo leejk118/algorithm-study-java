1021eca470dd594db91cc68ec31c9709b4247ed3
leejk118 <leejk118@gmail.com>
2022-10-11 23:24:06 +0900

    [1003] 101

--- src/main/day1003/leetcode_101/leetcode_101_jklee.java
+++ src/main/day1003/leetcode_101/leetcode_101_jklee.java
@@ -2,10 +2,37 @@ package main.day1003.leetcode_101;
 
 import main.util.TreeNode;
 
-public class leetcode_101_jklee {
-    public boolean isSymmetric(TreeNode root) {
+import java.util.ArrayList;
+import java.util.List;
 
-        return true;
+public class leetcode_101_jklee {
+    private List<Integer> left, right;
+
+    public boolean isSymmetric(TreeNode root) {
+        left = new ArrayList<Integer>();
+        right = new ArrayList<Integer>();
+
+        preorder(root.left);
+        postorder(root.right);
+        return left.equals(right);
+    }
+    private void preorder(TreeNode node) {
+        if (node == null) {
+            left.add(null);
+            return;
+        }
+        preorder(node.left);
+        left.add(node.val);
+        preorder(node.right);
+    }
+    private void postorder(TreeNode node) {
+        if (node == null) {
+            right.add(null);
+            return;
+        }
+        postorder(node.right);
+        right.add(node.val);
+        postorder(node.left);
     }
 
 
