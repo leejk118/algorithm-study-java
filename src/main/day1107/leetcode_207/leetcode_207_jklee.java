87452403568ecc058b33d2c6c74cc0e00446ab68
leejk118 <leejk118@gmail.com>
2022-11-16 01:18:42 +0900

    day1107: 207

--- src/main/day1107/leetcode_207/leetcode_207_jklee.java
+++ src/main/day1107/leetcode_207/leetcode_207_jklee.java
@@ -4,52 +4,50 @@ import java.util.*;
 
 public class leetcode_207_jklee {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean answer = true;
-        ArrayList<HashSet<Integer>> courseInfo = new ArrayList<>();
-        for (int i = 1; i <= numCourses; ++i) {
-            courseInfo.add(new HashSet<>());
-        }
+        ArrayList<Integer>[] courseInfo = new ArrayList[numCourses];
+        Stack<Integer> st = new Stack<>();
+        int[] degree = new int [numCourses];
 
+        for (int i = 0; i < numCourses; ++i) {
+            courseInfo[i] = new ArrayList<>();
+        }
         for (int[] pre : prerequisites) {
-            HashSet<Integer> set = courseInfo.get(pre[0]);
-            set.add(pre[1]);
-            courseInfo.set(pre[0], set);
+            courseInfo[pre[1]].add(pre[0]);
+            degree[pre[0]]++;
         }
 
-        outerloop: for (int i = 0; i < numCourses; ++i) {
-            HashSet<Integer> currentSet = new HashSet<>(), nextSet = new HashSet<>();
-            for (int course : courseInfo.get(i)) {
-                currentSet.add(course);
-            }
-            while (currentSet.size() > 0) {
-                for (int course : currentSet) {
-                    for (int c : courseInfo.get(course)) {
-                        nextSet.add(c);
-                    }
-                }
-                for (int course : nextSet) {
-                    if (currentSet.contains(course)) {
-                        answer = false;
-                        break outerloop;
-                    }
-                }
-                currentSet = new HashSet<>(nextSet);
+        for (int i = 0; i < degree.length; ++i) {
+            if (degree[i] == 0) {
+                st.add(i);
             }
         }
-        return answer;
+        int count = 0;
+        while (!st.empty()) {
+            int course = st.pop();
+            for (int c : courseInfo[course]) {
+                degree[c]--;
+                if (degree[c] == 0) {
+                    st.add(c);
+                }
+            }
+            count++;
+        }
+        return count == numCourses;
     }
 
 
     public static void main(String[] args) {
-        int[] numCourses = {5}; //{2, 2, 4, 4, 3, 5};
+        int[] numCourses = {2, 2, 4, 4, 3, 5, 7};
         int[][][] prerequisites = {
-//                {{1, 0}},
-//                {{1, 0}, {0, 1}},
-//                {{3, 2}, {2, 1}, {1, 0}},
-//                {{3, 2}, {2, 1}, {1, 3}},
-//                {{1, 0}, {1, 2}, {0, 1}},
-                {{1, 4}, {2, 4}, {3, 1}, {3, 2}}
+                {{1, 0}},
+                {{1, 0}, {0, 1}},
+                {{3, 2}, {2, 1}, {1, 0}},
+                {{3, 2}, {2, 1}, {1, 3}},
+                {{1, 0}, {1, 2}, {0, 1}},
+                {{1, 4}, {2, 4}, {3, 1}, {3, 2}},
+                {{1, 0}, {0, 3}, {0, 2}, {3, 2}, {2, 5}, {4, 5}, {5, 6}, {2, 4}}
         };
+
         for (int i = 0; i < numCourses.length; ++i) {
             System.out.println(new leetcode_207_jklee().canFinish(numCourses[i], prerequisites[i]));
         }
